995bdd10b955e578e72e11ef3519bcf77fe7ec9a
Fadros <260285883+Fadros@users.noreply.github.com>
2026-02-21 20:29:03 -0500

    started js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World.");
+const timer = getElementById("timer");
+let fulltimer = 1500;
+
+let minutes = Math.floor(fulltimer / 60);
+let seconds = fulltimer % 60;
+
+let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+let displayTime = formattedMinutes + ":" + formattedSeconds;
+timer.innerText = displayTime;
