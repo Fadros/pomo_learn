6fa87942ff617154206baaf84216763ea3d1199b
Fadros <260285883+Fadros@users.noreply.github.com>
2026-02-22 19:36:23 -0500

    Script location change and some js startup change

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const timer = getElementById("timer");
-let fulltimer = 1500;
+let remainingTime = 1500;
+let intervalId = null;
 
+function updateDisplay() {
+const timer = document.getElementById("timer");
 let minutes = Math.floor(fulltimer / 60);
 let seconds = fulltimer % 60;
 
@@ -9,3 +11,4 @@ let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
 
 let displayTime = formattedMinutes + ":" + formattedSeconds;
 timer.innerText = displayTime;
+}
