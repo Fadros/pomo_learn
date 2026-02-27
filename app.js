8eb9c4740f17b880717277fc01588ef0a5bcf225
Fadros <260285883+Fadros@users.noreply.github.com>
2026-02-27 13:43:19 -0500

    Replaced fulltimer

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let intervalId = null;
 
 function updateDisplay() {
 const timer = document.getElementById("timer");
-let minutes = Math.floor(fulltimer / 60);
-let seconds = fulltimer % 60;
+let minutes = Math.floor(remainingTime / 60);
+let seconds = remainingTime % 60;
 
 let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
 let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
