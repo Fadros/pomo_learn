2d2a44d4c1a55a6a5c7442e9919b0607e6e851f0
Fadros <260285883+Fadros@users.noreply.github.com>
2026-02-27 14:14:42 -0500

    Added start buttons animations

--- app.js
+++ app.js
@@ -12,3 +12,45 @@ let formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
 let displayTime = formattedMinutes + ":" + formattedSeconds;
 timer.innerText = displayTime;
 }
+
+function startTimer() {
+
+  const startButton = document.getElementById("bigstart");
+
+  // If timer is running → STOP it
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.innerText = "START";
+    return;
+  }
+
+  // Otherwise → START it
+  intervalId = setInterval(function() {
+
+    remainingTime--;
+    updateDisplay();
+
+    if (remainingTime <= 0) {
+      clearInterval(intervalId);
+      intervalId = null;
+      startButton.innerText = "START";
+        
+      const sound = document.getElementById("alarmSound");
+        sound.play();
+
+        startButton.classList.add("shake");
+
+        setTimeout(function() {
+            startButton.classList.remove("shake");
+        }, 1200);
+    }
+
+  }, 1000);
+
+  startButton.innerText = "STOP";
+}
+const startButton = document.getElementById("bigstart");
+startButton.addEventListener("click", startTimer);
+
+updateDisplay();
